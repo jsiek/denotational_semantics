8edeb4c10cc699fa29bafdc8e600966825720d02
Jeremy Siek <jsiek@129-79-241-203.dhcp-bl.indiana.edu>
2018-06-08 15:41:16 -0400

    remove lookup

--- DenotSem/Lookup.thy
+++
@@ -1,9 +0,0 @@
-theory Lookup
-  imports Main
-begin
-
-fun lookup :: "('a \<times> 'b) list \<Rightarrow> 'a \<Rightarrow> 'b option" where
-  "lookup [] a = None" |
-  "lookup ((b,c)#L) a = (if a = b then Some c else lookup L a)"
-
-end
