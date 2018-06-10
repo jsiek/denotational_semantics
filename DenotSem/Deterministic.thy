9fd09670d460fbdae42d0876ee1f210e2e3083f6
Jeremy Siek <jsiek@indiana.edu>
2018-06-10 11:05:08 -0400

    check

--- DenotSem/Deterministic.thy
+++ DenotSem/Deterministic.thy
@@ -36,7 +36,21 @@ next
   then show ?case sorry
 next
   case (EApp e1 e2)
-  then show ?case sorry
+  obtain f1 v21 where v_v1: "is_val v1" and f1_e1: "VFun f1 \<in> \<lbrakk>e1\<rbrakk>\<rho>1" and
+    v21_e2: "v21 \<in> \<lbrakk>e2\<rbrakk>\<rho>1" and v21v1_f1: "[(v21, v1)] \<sqsubseteq> f1" using EApp.prems(1) by auto
+  obtain f2 v22 where v_v2: "is_val v2" and f2_e1: "VFun f2 \<in> \<lbrakk>e1\<rbrakk>\<rho>2" and
+    v22_e2: "v22 \<in> \<lbrakk>e2\<rbrakk>\<rho>2" and v22v2_f2: "[(v22, v2)] \<sqsubseteq> f2" using EApp.prems(2) by auto
+  have f1_f2: "VFun f1 ~ VFun f2"
+    using EApp.IH(1) EApp.prems(3) EApp.prems(4) EApp.prems(5) f1_e1 f2_e1 by blast
+  have cf12: "VFun f1 ~ VFun f2" using EApp.IH(1)[of "VFun f1" \<rho>1 "VFun f2"] f1_f2 by blast
+  obtain vf12 \<rho>3 where
+    f12_vf12: "VFun f1 \<squnion> VFun f2 = Some vf12" and 
+    v_vf12: "is_val vf12" and r12_r3: "\<rho>1 \<squnion> \<rho>2 = Some \<rho>3" and v_r3: "val_env \<rho>3" and
+    vf12_e2: "vf12 \<in> \<lbrakk>e1\<rbrakk>\<rho>3" using EApp.IH(1)[of "VFun f1" \<rho>1 "VFun f2" \<rho>2] EApp.prems(3) 
+      EApp.prems(4) EApp.prems(5) f1_e1 f2_e1 by blast 
+  
+    
+  show ?case sorry
 next
   case (EPrim f e1 e2)
   then show ?case sorry
