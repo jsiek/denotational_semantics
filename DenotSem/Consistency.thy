7dbcc51ed8f4a8f1d3442dc3ad1c59e697189014
Jeremy Siek <jsiek@indiana.edu>
2018-06-14 08:25:24 -0400

    check

--- DenotSem/Consistency.thy
+++ DenotSem/Consistency.thy
@@ -21,6 +21,13 @@ inductive_cases
   nv_nat_inv[elim!]: "not_val (VNat n)" and
   nv_fun_inv: "not_val (VFun f)"
 
+abbreviation consistent :: "val \<Rightarrow> val \<Rightarrow> bool" (infix "~" 53) where
+  "v1 ~ v2 \<equiv> (\<exists> v3. v1 \<squnion> v2 = Some v3 \<and> is_val v3)"
+abbreviation is_fun :: "func \<Rightarrow> bool" where
+  "is_fun f \<equiv>  \<forall> v1 v1' v2 v2'. (v1,v1') \<in> set f \<and> (v2,v2') \<in> set f \<and> v1 ~ v2 \<longrightarrow> v1' ~ v2'"
+abbreviation has_vals :: "func \<Rightarrow> bool" where
+  "has_vals f \<equiv> \<forall> v v'. (v,v') \<in> set f \<longrightarrow> is_val v \<and> is_val v'"
+
 lemma is_val_and_not_val: "(is_val v \<longrightarrow> \<not> (not_val v)) \<and> (not_val v \<longrightarrow> \<not> is_val v)"
   apply (induction rule: is_val_not_val.induct)
   apply blast
@@ -30,19 +37,31 @@ lemma is_val_and_not_val: "(is_val v \<longrightarrow> \<not> (not_val v)) \<and
   using v_fun_inv apply blast 
   done
 
+lemma consis_join_val:
+  assumes v1_v2: "v1 ~ v2" and v_v1: "is_val v1" and v_v2: "is_val v2"
+  shows "\<exists> v12. (v1 \<squnion> v2) = Some v12 \<and> is_val v12"
+  using v1_v2 v_v1 v_v2 by auto
+
+lemma is_val_or_not_aux: "\<forall> v. n = vsize v \<longrightarrow> is_val v \<or> not_val v"
+  apply (induction n rule: nat_less_induct)
+  apply (rule allI) apply (rule impI)
+  apply (case_tac v)
+  apply auto
+  apply (metis add_lessD1 less_SucI size_fun_mem vfun_not_val1)
+  apply (metis add.commute add_lessD1 less_SucI size_fun_mem vfun_not_val2)    
+  apply (subgoal_tac "vsize v1 < Suc (fsize x2)") prefer 2 using size_fun_mem 
+    apply (meson add_lessD1 less_SucI)
+  apply (subgoal_tac "vsize v2 < Suc (fsize x2)") prefer 2 using size_fun_mem 
+    apply (meson add_lessD1 less_SucI)
+  apply (subgoal_tac "is_val v1 \<or> not_val v1") prefer 2 apply force
+  apply (subgoal_tac "is_val v2 \<or> not_val v2") prefer 2 apply force
+  oops    
+    
 lemma is_val_or_not: "is_val v \<or> not_val v" sorry
 
 lemma not_val_is_val[simp]: "not_val v = (\<not> is_val v)"
   using is_val_and_not_val is_val_or_not by blast
     
-abbreviation consistent :: "val \<Rightarrow> val \<Rightarrow> bool" (infix "~" 53) where
-  "v1 ~ v2 \<equiv> (\<exists> v3. v1 \<squnion> v2 = Some v3 \<and> is_val v3)"
-
-abbreviation is_fun :: "func \<Rightarrow> bool" where
-  "is_fun f \<equiv>  \<forall> v1 v1' v2 v2'. (v1,v1') \<in> set f \<and> (v2,v2') \<in> set f \<and> v1 ~ v2 \<longrightarrow> v1' ~ v2'"
-    
-abbreviation has_vals :: "func \<Rightarrow> bool" where
-  "has_vals f \<equiv> \<forall> v v'. (v,v') \<in> set f \<longrightarrow> is_val v \<and> is_val v'"
   
 lemma is_fun_intro[intro!]: "\<lbrakk> is_fun f; \<forall> v v'. (v,v') \<in> set f \<longrightarrow> is_val v \<and> is_val v' \<rbrakk>
        \<Longrightarrow> is_val (VFun f)" 
@@ -66,10 +85,6 @@ lemma inconsis_le: assumes v1p_v2p: "\<not>(v1' ~ v2')" and v1_v1p: "v1' \<sqsub
   shows "\<not>(v1 ~ v2)"
   sorry
 
-lemma consis_join_val:
-  assumes v1_v2: "v1 ~ v2" and v_v1: "is_val v1" and v_v2: "is_val v2"
-  shows "\<exists> v12. (v1 \<squnion> v2) = Some v12 \<and> is_val v12"
-  using v1_v2 v_v1 v_v2 by auto
   
     
 end
